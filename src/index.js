e09c80832d46134815a4414cc40c438f8f39890f
Javier Pena <javier@javierdev.com>
2016-10-13 11:01:58 -0500

    Foundation import fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 import 'angular-ui-router';
 import 'angular-foundation';
-import 'foundation-sites/css/normalize.css';
-import 'foundation-sites/css/foundation.css';
 
 import { app, router } from './app';
 import about from './app/about';
