ef2299a304348b5544bc3a51e66b683ca1103e98
Javier Pena <javier@javierdev.com>
2016-10-17 11:34:28 -0500

    Add comment for protractor firefox config

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,6 +4,17 @@ require('babel-register');
 exports.config = {
   baseUrl: 'http://localhost:8080',
   specs: ['./src/**/*.e2e.js'],
+  multiCapabilities: [
+    {
+      'browserName': 'chrome'
+    },
+    // Current firefox version does not navigate to page. Disable until problem
+    // fixed, though it seems to be a reocurring issue
+    // See: https://github.com/angular/protractor/issues/3150
+    // {
+    //   'browserName': 'firefox'
+    // }
+  ],
 	onPrepare: function() {
 		browser.manage().window().setSize(1280, 1024);
 	}
