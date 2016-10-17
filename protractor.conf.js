323edd71d420a772a6fd3c837ff4319fbd494ea3
Javier Pena <javier@javierdev.com>
2016-10-17 17:23:02 -0500

    Add sauce addon to travis build for e2e testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,33 @@
-var capabilities = {
-  'browserName': 'chrome'
-};
-
-if (process.env.TRAVIS) {
-  capabilities.chromeOptions = {
-    'args': ['--no-sandbox'],
-    version: 53
-  };
-}
-
 // Babel required for es6 code in e2e tests
 require('babel-register');
 
-exports.config = {
+let config = {
   baseUrl: 'http://localhost:8080',
   specs: ['./src/**/*.e2e.js'],
-  capabilities: capabilities
+  multiCapabilities: [
+    {
+      'browserName': 'chrome'
+    },
+    // Current firefox version does not navigate to page. Disable until problem
+    // fixed, though it seems to be a reocurring issue
+    // See: https://github.com/angular/protractor/issues/3150
+    // {
+    //   'browserName': 'firefox'
+    // }
+  ],
+	onPrepare: function() {
+		browser.manage().window().setSize(1280, 1024);
+	}
 };
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
+
+exports.config = config;
