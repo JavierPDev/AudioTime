b02330e4b396d9dc5c8906165caa3b2786c86aad
Javier Pena <javier@javierdev.com>
2016-10-17 17:28:41 -0500

    Protractor config edit for sauce + travis

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,17 +4,15 @@ require('babel-register');
 let config = {
   baseUrl: 'http://localhost:8080',
   specs: ['./src/**/*.e2e.js'],
-  multiCapabilities: [
-    {
-      'browserName': 'chrome'
-    },
-    // Current firefox version does not navigate to page. Disable until problem
-    // fixed, though it seems to be a reocurring issue
-    // See: https://github.com/angular/protractor/issues/3150
-    // {
-    //   'browserName': 'firefox'
-    // }
-  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  // Current firefox version does not navigate to page. Disable until problem
+  // fixed, though it seems to be a reocurring issue
+  // See: https://github.com/angular/protractor/issues/3150
+  // {
+  //   'browserName': 'firefox'
+  // }
 	onPrepare: function() {
 		browser.manage().window().setSize(1280, 1024);
 	}
