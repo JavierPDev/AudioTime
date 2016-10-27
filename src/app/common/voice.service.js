087026d27752ffd6132f6c2de7409a9502b2a753
Javier Pena <javier@javierdev.com>
2016-10-26 20:17:04 -0500

    #1 Reorder methods

--- src/app/common/voice.service.js
+++ src/app/common/voice.service.js
@@ -12,6 +12,41 @@ export default class VoiceService {
   }
 
   /**
+   * Get time as a phrase. Return a number unless input is a minute ie. 2:00
+   * @param {Number} time - Time in seconds
+   * @return {Any} Time as phrase. Either number eg. 3 or string eg. '1 minute'
+   */
+  getTimePhrase(time) {
+    let phrase;
+    let isMin = time % 60;
+    if (time < 60) {
+      phrase = time;
+    } else if (isMin === 0) {
+      let min = time / 60;
+      let isSingular = min === 1;
+      phrase = isSingular ? min + ' minute' : min + ' minutes';
+    } else {
+      phrase = this._$filter('formatTime')(time);
+    }
+    return phrase;
+  }
+
+  listen() {
+    this._recognition.start();
+    console.log('now listening');
+  }
+
+  stopListening() {
+    this._recognition.stop();
+    console.log('stopped listening');
+  }
+
+  speak(phrase) {
+    this._speechMsg.text = phrase;
+    window.speechSynthesis.speak(this._speechMsg);
+  }
+
+  /**
    * Set up voice recognition process using webkit's api
    */
   _setupRecognition() {
@@ -53,40 +88,5 @@ export default class VoiceService {
     this._speechMsg.rate = 2.5;
     this._speechMsg.lang = 'en-US';
     this._speechMsg.voice = voices[1];
-  }
-
-  listen() {
-    this._recognition.start();
-    console.log('now listening');
-  }
-
-  stopListening() {
-    this._recognition.stop();
-    console.log('stopped listening');
-  }
-
-  speak(phrase) {
-    this._speechMsg.text = phrase;
-    window.speechSynthesis.speak(this._speechMsg);
-  }
-
-  /**
-   * Get time as a phrase. Return a number unless input is a minute ie. 2:00
-   * @param {Number} time - Time in seconds
-   * @return {Any} Time as phrase. Either number eg. 3 or string eg. '1 minute'
-   */
-  getTimePhrase(time) {
-    let phrase;
-    let isMin = time % 60;
-    if (time < 60) {
-      phrase = time;
-    } else if (isMin === 0) {
-      let min = time / 60;
-      let isSingular = min === 1;
-      phrase = isSingular ? min + ' minute' : min + ' minutes';
-    } else {
-      phrase = this._$filter('formatTime')(time);
-    }
-    return phrase;
   }
 }
