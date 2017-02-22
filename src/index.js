e500a64c9fe23f0ac9e9a1b8e2f8d24a502818ea
Javier Pena <javier@javierdev.com>
2017-02-22 11:52:58 -0600

    Add notice for nonwebkit users

--- src/index.js
+++ src/index.js
@@ -12,16 +12,38 @@ import stopwatch from './app/stopwatch';
 import timer from './app/timer';
 import './main.scss';
 
-const appModule = angular.module('app', [
-  ngAnimate,
-  'ui.router',
-  'mm.foundation',
-]);
+const speechCapable = window.webkitSpeechRecognition;
+const utteranceCapable = window.speechSynthesis;
 
-app(appModule);
-common(appModule);
-about(appModule);
-settings(appModule);
-stopwatch(appModule);
-timer(appModule);
-router(appModule);
+if (speechCapable && utteranceCapable) {
+  bootstrapApp();
+} else {
+  notifyUserOfBrowserRequirements();
+}
+
+function bootstrapApp() {
+  const appModule = angular.module('app', [
+    ngAnimate,
+    'ui.router',
+    'mm.foundation',
+  ]);
+
+  app(appModule);
+  common(appModule);
+  about(appModule);
+  settings(appModule);
+  stopwatch(appModule);
+  timer(appModule);
+  router(appModule);
+}
+
+function notifyUserOfBrowserRequirements() {
+  const spinner = document.querySelector('.loading-spinner');
+  const div = document.createElement('div');
+  spinner.remove();
+  div.innerText = 'Google Chrome/Webkit Speech Recognition and Speech Utterance'
+    +' required. Please try again with Google Chrome. Some versions of Chrome,'
+    +' such as Chrome for iOS, may not work.';
+  div.className = 'alert-box warning';
+  document.body.appendChild(div);
+}
